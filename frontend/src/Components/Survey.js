fb0d12eb7a8b7d2d513c407d251bb79c043c80bf
Nour Mshawrab <mshawrabnour@gmail.com>
2022-06-19 03:17:34 +0300

    Creating survey form

---
+++ frontend/src/Components/Survey.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+function Survey(){
+    return(
+        <div className="surveyContainer">
+            <input className="title" placeholder='Insert title here'></input>
+            <input className="description" placeholder='Insert Description'></input>
+            <button className="create-btn ques" >Add Question</button>
+
+
+        </div>
+        
+    )
+}
+
+
+export default Survey
--- frontend/src/Pages/Admin.js
+++ frontend/src/Pages/Admin.js
@@ -1,8 +1,11 @@
+import Survey from '../Components/Survey'
+
 const Profile = () => {
     return (
     <div className="adminAccess">
         <h1>Welcome, Admin</h1>
         <button className="create-btn" >Create Questionnaire</button>
+        <Survey/>
     </div>
     )
    };
