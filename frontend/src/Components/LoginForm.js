69fe2d5dc1fe55b4b05321a91d4bd85dc4281878
Nour Mshawrab <mshawrabnour@gmail.com>
2022-06-21 04:19:27 +0300

    Linking Login

--- frontend/src/Components/LoginForm.js
+++ frontend/src/Components/LoginForm.js
@@ -1,6 +1,21 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 function LoginForm() {
+  const[myEmail,setMyEmail]=useState("");
+  const[myPassword,setMyPassword]=useState("");
+
+  const handleSubmit = (e)=> {
+    const data=new FormData
+    data.append('email',myEmail)
+    data.append('password',myPassword)
+    e.preventDefault();
+      axios({
+        method:"post",
+        data,
+        url:"http://127.0.0.1:8000/api/login",
+      }).then(res=>console.log(res)).catch(error=>console.log(error))
+  }
   
     return (
       <div className="container">
@@ -10,14 +25,17 @@ function LoginForm() {
             <label>Email:</label>
             <input
               placeholder='Enter Email'
+              value={myEmail} onChange={(e)=>setMyEmail(e.target.value)}
         
             />
             <label>Password:</label>
             <input
               placeholder='Enter Password'
+              type="password"
+              value={myPassword} onChange={(e)=>setMyPassword(e.target.value)}
            
             />
-            <button className="btn orange">Login</button>
+            <button className="btn orange" onClick={handleSubmit}>Login</button>
           
         </form>
       </div>
